ad1f6019fe7c33f7110f876a1179eb252b705d3b
ambiennt <ambienntdev@gmail.com>
2022-06-29 15:05:32 -0700

    small micro-optimization

--- src/signature_scanner.cpp
+++ src/signature_scanner.cpp
@@ -15,20 +15,21 @@ uintptr_t mem::findSignatureDefault(const std::vector<int16_t>& bytes) {
 	if (bytes.size() <= 1) { return 0; }
 
 	uint16_t patternSize = (uint16_t)bytes.size();
-	uint16_t matchLength = patternSize - 1;
-	const int16_t* front = bytes.data();
+	uint16_t matchLength = (patternSize - 1);
+	const int16_t* sig = bytes.data();
+	uintptr_t scanSpan = (mem::MODULE_END - patternSize);
 
-	for (uintptr_t pCur = mem::MODULE_BASE; pCur < mem::MODULE_END; pCur++) {
+	for (uintptr_t pCur = mem::MODULE_BASE; pCur < scanSpan; pCur++) {
 
-		if (*(int8_t*)pCur == (int8_t)front[0]) { // if first opcode matches
+		if (*(int8_t*)pCur == (int8_t)sig[0]) { // if first opcode matches
 			
-			if (*(int8_t*)(pCur + matchLength) == (int8_t)front[matchLength]) { // if last opcode matches
+			if (*(int8_t*)(pCur + matchLength) == (int8_t)sig[matchLength]) { // if last opcode matches
 
-				for (uint16_t gamer = 1; gamer < patternSize; gamer++) {
+				for (uint16_t gamer = 1; gamer < patternSize; gamer++) { // we already checked first sig opcode above so lets start at 1
 
-					if (front[gamer] == -1) continue; // -1 in the bytes vector is considered a wildcard
+					if (sig[gamer] == -1) continue; // -1 in the bytes vector is considered a wildcard
 
-					if (*(int8_t*)(pCur + gamer) == (int8_t)front[gamer]) {
+					if (*(int8_t*)(pCur + gamer) == (int8_t)sig[gamer]) {
 						if (gamer == matchLength) { return pCur; }
 						else continue;
 					}
